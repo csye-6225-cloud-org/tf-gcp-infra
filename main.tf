b69ca91ca9a997601134d4b4db1b830179a1a6d6
Anuraag Bathula <bathula.anuraag.100@gmail.com>
2024-02-22 08:35:32 -0500

    Adding code for creation of firewall and gce vm

--- main.tf
+++ main.tf
@@ -8,19 +8,19 @@ terraform {
 }
 
 provider "google" {
-  #   credentials = "/Users/anuraag/.config/gcloud/application_default_credentials.json"
-  credentials = "gcp-creds.json"
-  project     = var.gcp_project
-  region      = var.region
+  credentials = "/Users/anuraag/.config/gcloud/application_default_credentials.json"
+  # credentials = "gcp-creds.json"
+  project = var.gcp_project
+  region  = var.region
 }
 
 resource "google_compute_network" "vpc_network" {
   count                           = length(var.vpc_names)
   name                            = var.vpc_names[count.index]
   project                         = var.gcp_project
-  auto_create_subnetworks         = false
-  routing_mode                    = "REGIONAL"
-  delete_default_routes_on_create = true
+  auto_create_subnetworks         = var.auto_create_subnetworks
+  routing_mode                    = var.vpc_routing_mode
+  delete_default_routes_on_create = var.delete_default_routes_on_create
 }
 
 resource "google_compute_subnetwork" "vpc_subnet_1" {
@@ -46,4 +46,65 @@ resource "google_compute_route" "network-route" {
   network          = google_compute_network.vpc_network.*.name[count.index]
   next_hop_gateway = "default-internet-gateway"
   priority         = 100
+}
+
+resource "google_compute_firewall" "internet_ingress_firewall_deny" {
+  count = length(var.vpc_names)
+  priority = 101
+  name    = "internet-ingress-firewall-deny-${count.index + 1}"
+  network = google_compute_network.vpc_network.*.name[count.index]
+  deny {
+    protocol = "all"
+  }
+  destination_ranges = [var.webapp_cidr_range[count.index]]
+  # 35.235.240.0/20
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["webapp-server"]
+}
+
+resource "google_compute_firewall" "internet_ingress_firewall_allow" {
+  count = length(var.vpc_names)
+  priority = 100
+  name    = "internet-ingress-firewall-allow-${count.index + 1}"
+  network = google_compute_network.vpc_network.*.name[count.index]
+  allow {
+    protocol = "tcp"
+    ports    = ["8080", "22"]
+  }
+  destination_ranges = [var.webapp_cidr_range[count.index]]
+  # 35.235.240.0/20
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["webapp-server"]
+}
+
+resource "google_compute_instance" "tf_instance" {
+  name         = "tf-instance"
+  machine_type = "e2-medium"
+  zone  = "us-east1-b"
+
+  tags = ["webapp-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "packer-1708599626"
+      type = "pd-balanced"
+      size = 100
+      labels = {
+        # name = "webapp-server"
+        my_label = "value"
+      }
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network[0].name
+    subnetwork = google_compute_subnetwork.vpc_subnet_1[0].name
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,47 @@
 variable "gcp_project" {
-  type = string
-  default = "csye-6225-gcp-project"
+  type    = string
+  default = "csye-6225-project-dev"
 }
 
 variable "vpc_names" {
-  type = list(any)
+  type    = list(any)
   default = ["tf-network-1", "tf-network-2"]
 }
 
 variable "region" {
-  type = string
-    default = "us-east1"
+  type    = string
+  default = "us-east1"
 }
 
 variable "webapp_cidr_range" {
-  type = list(any)
+  type    = list(any)
   default = ["10.0.0.0/24", "10.0.1.0/24"]
 }
 
 variable "db_cidr_range" {
-  type = list(any)
+  type    = list(any)
   default = ["172.16.0.0/24", "172.16.1.0/24"]
 }
 
 variable "route_destination" {
-  type = string
-    default = "0.0.0.0/0"
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "vpc_routing_mode" {
+  type    = string
+  default = "REGIONAL"
+}
+
+variable "auto_create_subnetworks" {
+  type    = bool
+  default = false
+}
+variable "delete_default_routes_on_create" {
+  type    = bool
+  default = true
+}
+variable "firewall" {
+  type    = list(any)
+  default = ["172.16.0.0/24", "172.16.1.0/24"]
 }
